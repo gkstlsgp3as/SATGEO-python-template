c42dbc6b82454cd8eafd89156b64c42b529d85e4
gkstlsgp3as <58411517+gkstlsgp3as@users.noreply.github.com>
2024-12-28 00:42:51 +0900

    Update sample_model.py

--- app/models/sample_model.py
+++ app/models/sample_model.py
@@ -13,3 +13,9 @@ class SampleModel(Base): # 수정
     field1: Mapped[type] = mapped_column(primary_key=True, index=True) # 필드명 및 타입 변경, Primary Key 작성
     field2: Mapped[type] = mapped_column() # 필드명 및 타입 변경
     ...
+
+## 필드 종류 예시
+    dates: Mapped[List[str]] = mapped_column(ARRAY(String))            # List[str]
+    status: Mapped[str] = mapped_column(nullable=False)                # null값 비허용
+    try_number: Mapped[int] = mapped_column(default=0)                 # 디폴트 값 정의 
+    created_at: Mapped[datetime] = mapped_column(TIMESTAMP, nullable=False)    # 시간
