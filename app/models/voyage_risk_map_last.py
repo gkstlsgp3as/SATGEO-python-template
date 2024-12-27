7c6c40692d51d2c583f0b69a7fca4ea982044138
gkstlsgp3as <58411517+gkstlsgp3as@users.noreply.github.com>
2024-12-28 00:37:55 +0900

    Create voyage_risk_map_last.py

---
+++ app/models/voyage_risk_map_last.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+from sqlalchemy.orm import mapped_column, Mapped
+
+from app.config.db_session import Base
+from decimal import Decimal
+
+
+class VoyageRiskMapLast(Base):
+    __tablename__ = "voyage_risk_map_last"
+    # __table_args__ = {'schema': 'public'}
+    __table_args__ = {'schema': 'gateway'}
+
+    longitude: Mapped[Decimal] = mapped_column(primary_key=True, index=True)
+    latitude: Mapped[Decimal] = mapped_column(primary_key=True, index=True)
+    voyage_risk_map_id: Mapped[str] = mapped_column()
+    longitude_length: Mapped[float] = mapped_column()
+    latitude_length: Mapped[float] = mapped_column()
+    wind_speed: Mapped[float] = mapped_column()
+    wind_direction: Mapped[float] = mapped_column()
+    wave_height: Mapped[float] = mapped_column()
+    wave_direction: Mapped[float] = mapped_column()
+    relative_humidity: Mapped[float] = mapped_column()
+    total_column_water_vapor: Mapped[float] = mapped_column()
+    skin_temperature: Mapped[float] = mapped_column()
+    tidal_current_speed: Mapped[float] = mapped_column()
+    tidal_current_direction: Mapped[float] = mapped_column()
+    typhoon_longitude: Mapped[float] = mapped_column()
+    typhoon_latitude: Mapped[float] = mapped_column()
+    typhoon_grade: Mapped[float] = mapped_column()
