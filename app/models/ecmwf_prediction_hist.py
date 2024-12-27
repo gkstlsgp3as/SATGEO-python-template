417cd6cea53f8e2c73dc9718f26a57e4fecaa7f7
gkstlsgp3as <58411517+gkstlsgp3as@users.noreply.github.com>
2024-12-28 00:36:39 +0900

    Create ecmwf_prediction_hist.py

---
+++ app/models/ecmwf_prediction_hist.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+from sqlalchemy.orm import mapped_column, Mapped
+
+from app.config.db_session import Base
+from decimal import Decimal
+
+
+class EcmwfPredictionHist(Base):
+    __tablename__ = "ecmwf_prediction_hist"
+    __table_args__ = {'schema': 'gateway'}
+
+    prediction_standard_time: Mapped[datetime] = mapped_column(primary_key=True, index=True)
+    prediction_time: Mapped[datetime] = mapped_column(primary_key=True, index=True)
+    longitude: Mapped[Decimal] = mapped_column(primary_key=True, index=True)
+    latitude: Mapped[Decimal] = mapped_column(primary_key=True, index=True)
+    longitude_length: Mapped[float] = mapped_column()
+    latitude_length: Mapped[float] = mapped_column()
+    wind_speed_real: Mapped[float] = mapped_column()
+    wind_direction_real: Mapped[float] = mapped_column()
+    wave_height_real: Mapped[float] = mapped_column()
+    wave_direction_real: Mapped[float] = mapped_column()
+    relative_humidity: Mapped[float] = mapped_column()
+    total_column_water_vapor: Mapped[float] = mapped_column()
+    skin_temperature: Mapped[float] = mapped_column()
