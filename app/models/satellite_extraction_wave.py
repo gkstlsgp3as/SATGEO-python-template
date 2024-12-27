3209f85185b4dde5b7f984f855876e604941e801
gkstlsgp3as <58411517+gkstlsgp3as@users.noreply.github.com>
2024-12-28 01:01:18 +0900

    Create satellite_extraction_wave.py

---
+++ app/models/satellite_extraction_wave.py
@@ -0,0 +1,17 @@
+from sqlalchemy.orm import mapped_column, Mapped
+
+from app.config.db_session import Base
+from decimal import Decimal
+
+class SatelliteExtractionWave(Base):
+    __tablename__ = "satellite_extraction_wave"
+
+    satellite_eo_image_id: Mapped[str] = mapped_column(primary_key=True, index=True)
+    longitude: Mapped[Decimal] = mapped_column(primary_key=True, index=True)
+    latitude: Mapped[Decimal] = mapped_column(primary_key=True, index=True)
+    longitude_length: Mapped[float] = mapped_column(Float)
+    latitude_length: Mapped[float] = mapped_column(Float)
+    wave_length: Mapped[float] = mapped_column(Float)
+    azimuth_cutoff: Mapped[float] = mapped_column(Float)
+    mean_wave_height: Mapped[float] = mapped_column(Float)
+    mean_wave_direction: Mapped[float] = mapped_column(Float)
