7f5c1ed6311efdb10fda2e36c04ad7dbf0a1bae3
gkstlsgp3as <58411517+gkstlsgp3as@users.noreply.github.com>
2024-12-28 00:58:53 +0900

    Update kma_prediction_hist.py

--- app/models/kma_prediction_hist.py
+++ app/models/kma_prediction_hist.py
@@ -1,19 +1,18 @@
 from datetime import datetime
-from sqlalchemy import Float, Numeric
-from sqlalchemy.dialects.postgresql import TIMESTAMP
+
 from sqlalchemy.orm import mapped_column, Mapped
 
 from app.config.db_session import Base
-
+from decimal import Decimal
 
 class KmaPredictionHist(Base):
   __tablename__ = "kma_prediction_hist"
   __table_args__ = {'schema': 'gateway'}
 
-  prediction_standard_time: Mapped[datetime] = mapped_column(TIMESTAMP, primary_key=True)
-  prediction_time: Mapped[datetime] = mapped_column(TIMESTAMP, primary_key=True)
-  longitude: Mapped[float] = mapped_column(Numeric(12, 8), primary_key=True)
-  latitude: Mapped[float] = mapped_column(Numeric(12, 8), primary_key=True)
+  prediction_standard_time: Mapped[datetime] = mapped_column(primary_key=True, index=True)
+  prediction_time: Mapped[datetime] = mapped_column(primary_key=True, index=True)
+  longitude: Mapped[Decimal] = mapped_column(primary_key=True, index=True)
+  latitude: Mapped[Decimal] = mapped_column(primary_key=True, index=True)
   wind_u_component: Mapped[float] = mapped_column(Float)
   wind_v_component: Mapped[float] = mapped_column(Float)
   large_scale_precipitation: Mapped[float] = mapped_column(Float)
