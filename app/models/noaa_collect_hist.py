5129f93fce07f2ed69228176215fb3bb484c7ddf
gkstlsgp3as <58411517+gkstlsgp3as@users.noreply.github.com>
2024-12-28 00:37:00 +0900

    Create noaa_collect_hist.py

---
+++ app/models/noaa_collect_hist.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from typing import List
+
+from sqlalchemy import ARRAY, String
+from sqlalchemy.dialects.postgresql import TIMESTAMP
+from sqlalchemy.orm import mapped_column, Mapped
+
+from app.config.db_session import Base
+
+
+class NoaaCollectHist(Base):
+    __tablename__ = "noaa_collect_hist"
+    __table_args__ = {'schema': 'gateway'}
+
+    transaction_id: Mapped[str] = mapped_column(primary_key=True, index=True)
+    request_dates: Mapped[List[str]] = mapped_column(ARRAY(String))
+    status: Mapped[str] = mapped_column(nullable=False)
+    task_id: Mapped[str] = mapped_column()
+    run_id: Mapped[str] = mapped_column()
+    try_number: Mapped[int] = mapped_column(default=0)
+    created_at: Mapped[datetime] = mapped_column(TIMESTAMP, nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(TIMESTAMP, nullable=False)
