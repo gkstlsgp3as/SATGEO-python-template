f26e3e6524378d5847e7adc6e75c60dfaed64224
gkstlsgp3as <58411517+gkstlsgp3as@users.noreply.github.com>
2024-12-28 17:16:18 +0900

    Create __init__.py

---
+++ app/algorithm/__init__.py
@@ -0,0 +1,7 @@
+from app.algorithm.w01.collect_ecmwf import w01_collect_ecmwf
+from app.algorithm.w01.collect_noaa import w01_collect_noaa
+from app.algorithm.w03.ready_to_use_ecmwf import w03_ready_to_use_ecmwf
+from app.algorithm.w03.ready_to_use_noaa import w03_ready_to_use_noaa
+from app.algorithm.w03.ready_to_use_noaa_cyclone import w03_ready_to_use_noaa_cyclone
+from app.algorithm.w07.calculate_correction_map import w07_calculate_correction_map
+from app.algorithm.w08.calculate_navigation_risk import w08_calculate_navigation_risk
