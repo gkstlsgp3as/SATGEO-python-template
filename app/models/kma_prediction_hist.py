c6b1f29b81015ad322474dcc37b728668857d05e
gkstlsgp3as <58411517+gkstlsgp3as@users.noreply.github.com>
2024-12-28 12:21:44 +0900

    Update kma_prediction_hist.py

--- app/models/kma_prediction_hist.py
+++ app/models/kma_prediction_hist.py
@@ -9,10 +9,10 @@ class KmaPredictionHist(Base):
   __tablename__ = "kma_prediction_hist"
   __table_args__ = {'schema': 'gateway'}
 
-  prediction_standard_time: Mapped[datetime] = mapped_column(primary_key=True, index=True)
-  prediction_time: Mapped[datetime] = mapped_column(primary_key=True, index=True)
-  longitude: Mapped[Decimal] = mapped_column(primary_key=True, index=True)
-  latitude: Mapped[Decimal] = mapped_column(primary_key=True, index=True)
+  prediction_standard_time: Mapped[datetime] = mapped_column(primary_key=True)
+  prediction_time: Mapped[datetime] = mapped_column(primary_key=True)
+  longitude: Mapped[Decimal] = mapped_column(primary_key=True)
+  latitude: Mapped[Decimal] = mapped_column(primary_key=True)
   wind_u_component: Mapped[float] = mapped_column(Float)
   wind_v_component: Mapped[float] = mapped_column(Float)
   large_scale_precipitation: Mapped[float] = mapped_column(Float)
