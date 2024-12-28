16241fea7c20e8258fdb03c93fa7d106735f051d
gkstlsgp3as <58411517+gkstlsgp3as@users.noreply.github.com>
2024-12-28 12:10:48 +0900

    Update sar_ship_identification.py

--- app/models/sar_ship_identification.py
+++ app/models/sar_ship_identification.py
@@ -6,14 +6,14 @@ class SarShipIdentification(Base):
   __tablename__ = "sar_ship_identification"
   __table_args__ = {'schema': 'gateway'}
 
-  satellite_sar_image_id: Mapped[str] = mapped_column(String(32), primary_key=True)
-  identification_ship_id: Mapped[str] = mapped_column(String(64), primary_key=True)
-  longitude: Mapped[float] = mapped_column(Numeric(12, 8))
-  latitude: Mapped[float] = mapped_column(Numeric(12, 8))
+  satellite_sar_image_id: Mapped[str] = mapped_column(primary_key=True, index=True)
+  identification_ship_id: Mapped[str] = mapped_column(primary_key=True, index=True)
+  longitude: Mapped[Decimal] = mapped_column()
+  latitude: Mapped[Decimal] = mapped_column()
   interpolation_cog: Mapped[float] = mapped_column(Float)
   interpolation_sog: Mapped[float] = mapped_column(Float)
-  type: Mapped[str] = mapped_column(String(10))
-  end: Mapped[str] = mapped_column(String(10))
+  type: Mapped[str] = mapped_column()
+  end: Mapped[str] = mapped_column()
   detection_yn: Mapped[bool] = mapped_column()
   detection_longitude: Mapped[float] = mapped_column(Float)
   detection_latitude: Mapped[float] = mapped_column(Float)
