a96fc89aae4c8a36a19db9b9ee3dd02fde77b099
gkstlsgp3as <58411517+gkstlsgp3as@users.noreply.github.com>
2024-12-28 16:41:54 +0900

    Update s05_classify_unidentified_ships.py

--- app/algorithm/s05/s05_classify_unidentified_ships.py
+++ app/algorithm/s05/s05_classify_unidentified_ships.py
@@ -2,10 +2,10 @@
 '''
 @Time          : 2024/12/18 00:00
 @Author        : Shinhye Han
-@File          : s05_classify_unmatched_ships.py
+@File          : s05_classify_unidentified_ships.py
 @Noice         : 
-@Description   : Perform multi-class classification on ship chips from SAR.
-@How to use    : python s05_classify_unmatched_ships.py --input_dir {image path} --meta_file {metafile path}
+@Description   : Perform multi-class classification on unidentified ship chips from SAR.
+@How to use    : python s05_classify_unidentified_ships.py --input_dir {image path} --meta_file {metafile path}
 
 @Modificattion :
     @Author    :
@@ -24,16 +24,15 @@ from torchvision import transforms
 
 from utils.datasets import ShipClsDataset
 from models.misc import select_model
-from cfg import ALGORITHM_NAME
+from utils.cfg import ALGORITHM_NAME
 
 ## TODO: 관련 DB 모델 및 서비스 import 
 from app.config.settings import settings
-from app.models.ecmwf_collect_hist import EcmwfCollectHist
-from app.service import ecmwf_collect_hist_service
+from app.models. import 
+from app.service import 
 
 
-
-def run(input_dir: str, output_dir: str, meta_file: str, classes: List[str], img_size: int) -> None:
+def classify_unidentified_ships(db: Session, satellite_sar_image_id: str) -> None:
     """
     Perform multi-class classification on ship images.
 
@@ -44,6 +43,10 @@ def run(input_dir: str, output_dir: str, meta_file: str, classes: List[str], img
         classes (List[str]): List of ship classes.
         img_size (int): Size to which images will be resized.
     """
+    input_dir = settings.S05_INPUT_PATH
+    output_dir = settings.S05_OUTPUT_PATH
+    meta_file = settings.S05_META_FILE
+    
     start_time = time.time()
 
     # Image Preprocessing
@@ -90,24 +93,15 @@ def run(input_dir: str, output_dir: str, meta_file: str, classes: List[str], img
     })
 
     ## TODO: 산출물을 DB 테이블 포맷으로 변경 -> 모델 활용
-    record = EcmwfPredictionHist(
-                prediction_standard_time=times[i],
-                prediction_time_timestamp=times[i],
-                latitude=Decimal(processed_data['latitude'][j].item()),
-                longitude=Decimal(processed_data['longitude'][k].item()),
-                latitude_length=float(processed_data['latitude'][j].item()),
-                longitude_length=float(processed_data['longitude'][k].item()),
-                wind_speed_real=float(processed_data['speed'][i, j, k].item()),
-                wind_direction_real=float(processed_data['dir'][i, j, k].item()),
-                wave_height_real=float(processed_data['waveh'][i, j, k].item()),
-                wave_direction_real=float(processed_data['waved'][i, j, k].item()),
-                relative_humidity=0.0,  # 필요한 값으로 교체
-                total_column_water_vapor=0.0,  # 필요한 값으로 교체
-                skin_temperature=0.0  # 필요한 값으로 교체
+    for i, pred in enumerate(predictions):
+        record = SarShipUnidentification(
+                satellite_sar_image_id=satellite_sar_image_id,
+                unidentification_ship_id=i,
+                
             )
             bulk_data.append(record)
 
-    results_df.to_csv(output_dir, index=False)
+        results_df.to_csv(output_dir, index=False)
 
     ## TODO: DB 모델에 컬럼 삽입할 서비스 호출
     # ecmwf_collect_hist_service.update_ecmwf_collect_history(db, collect_history)
@@ -129,5 +123,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
     args.classes = ['Cargo', 'Fishing', 'Sailing', 'Tanker', 'TugTow'] 
 
-    run(**vars(args)
+    classify_unidentified_ships(**vars(args)
 
