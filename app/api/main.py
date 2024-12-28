fe4aea1b97507bcd2d8b557049bb8b81fdd655df
gkstlsgp3as <58411517+gkstlsgp3as@users.noreply.github.com>
2024-12-28 20:03:55 +0900

    Update main.py

--- app/api/main.py
+++ app/api/main.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
-from app.api.routers import ships, risk_mappings
+from app.api.routers import ships, risk_mappings, 
 
 api_router = APIRouter()
 api_router.include_router(ships.router, prefix="/ships", tags=["S_ship"])
 api_router.include_router(risk_mappings.router, prefix="/risk-mappings", tags=["W_riskmapping"])
+api_router.include_router(coastal_monitorings.router, prefix="/coastal-monitorings", tags=["M_coastalmonitoring"])
+api_router.include_router(new_facilities.router, prefix="/new-facilities", tags=["N_newfacility"])
