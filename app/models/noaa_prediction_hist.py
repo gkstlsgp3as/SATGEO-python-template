0cef1a7660843d3da7312683f92a7e16697c2244
gkstlsgp3as <58411517+gkstlsgp3as@users.noreply.github.com>
2024-12-28 12:23:27 +0900

    Update noaa_prediction_hist.py

--- app/models/noaa_prediction_hist.py
+++ app/models/noaa_prediction_hist.py
@@ -9,25 +9,23 @@ class NoaaPredictionHist(Base):
     __tablename__ = "noaa_prediction_hist"
     __table_args__ = {'schema': 'gateway'}
 
-    prediction_standard_time: Mapped[datetime] = mapped_column(primary_key=True, index=True)
-    prediction_time: Mapped[datetime] = mapped_column(primary_key=True, index=True)
+    prediction_standard_time: Mapped[datetime] = mapped_column(primary_key=True)
+    prediction_time: Mapped[datetime] = mapped_column(primary_key=True)
 
-    # longitude_length: Mapped[float] = mapped_column(primary_key=True)
-    # latitude_length: Mapped[float] = mapped_column(primary_key=True)
     latitude: Mapped[float] = mapped_column(primary_key=True)
     longitude: Mapped[float] = mapped_column(primary_key=True)
 
-    pressure: Mapped[float] = mapped_column()
-    water_temperature: Mapped[float] = mapped_column()
+    pressure: Mapped[float] = mapped_column(Float)
+    water_temperature: Mapped[float] = mapped_column(Float)
 
-    current_speed_real: Mapped[float] = mapped_column()
-    current_direction_real: Mapped[float] = mapped_column()
+    current_speed_real: Mapped[float] = mapped_column(Float)
+    current_direction_real: Mapped[float] = mapped_column(Float)
 
-    wave_direction_real: Mapped[float] = mapped_column()
-    wave_period: Mapped[float] = mapped_column()
-    wave_height_real: Mapped[float] = mapped_column()
+    wave_direction_real: Mapped[float] = mapped_column(Float)
+    wave_period: Mapped[float] = mapped_column(Float)
+    wave_height_real: Mapped[float] = mapped_column(Float)
 
-    wind_direction_real: Mapped[float] = mapped_column()
-    wind_speed_real: Mapped[float] = mapped_column()
+    wind_direction_real: Mapped[float] = mapped_column(Float)
+    wind_speed_real: Mapped[float] = mapped_column(Float)
 
     update_date: Mapped[datetime] = mapped_column()
