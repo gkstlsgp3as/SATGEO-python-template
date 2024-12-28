7c0fb0f88746dbcaac03a452946a731cfd184939
gkstlsgp3as <58411517+gkstlsgp3as@users.noreply.github.com>
2024-12-28 12:32:00 +0900

    Update satellite_extraction_wind.py

--- app/models/satellite_extraction_wind.py
+++ app/models/satellite_extraction_wind.py
@@ -7,9 +7,9 @@ class SatelliteExtractionWind(Base):
   __tablename__ = "satellite_extraction_wind"
   __table_args__ = {'schema': 'gateway'}
 
-  satellite_eo_image_id: Mapped[str] = mapped_column(primary_key=True, index=True)
-  longitude: Mapped[Decimal] = mapped_column(primary_key=True, index=True)
-  latitude: Mapped[Decimal] = mapped_column(primary_key=True, index=True)
+  satellite_eo_image_id: Mapped[str] = mapped_column(primary_key=True)
+  longitude: Mapped[Decimal] = mapped_column(primary_key=True)
+  latitude: Mapped[Decimal] = mapped_column(primary_key=True)
   longitude_length: Mapped[float] = mapped_column(Float)
   latitude_length: Mapped[float] = mapped_column(Float)
   wind_speed: Mapped[float] = mapped_column(Float)
