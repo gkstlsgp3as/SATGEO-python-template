5eedc233d00c3e4acc58beb68559367c33f5dab9
gkstlsgp3as <58411517+gkstlsgp3as@users.noreply.github.com>
2024-12-31 19:27:26 +0900

    Update and rename kcgsa_dag_s_cls.py to kcgsa_dag_user_s.py

--- app/dags/kcgsa_dag_user_s.py
+++ app/dags/kcgsa_dag_user_s.py
@@ -7,30 +7,31 @@ from datetime import datetime
 host_path = 'http://ship-service.ship.svc.cluster.local:80/'
 
 
-def s05_api_call(execution_date, **kwargs):
-    api_path = '/api/ships/s05'
+def s07_api_call(execution_date, **kwargs):
+    api_path = '/api/ships/s07'
     api_params = {
-        'max-hour': 48
+        'satellite_sar_image_id': 123456789     # 사용자의 선박 클릭시 전송되는 인자 
+        'unidentification_ship_id': 01
     }
 
     requests.get(host_path + api_path, params=api_params)
 
 
 with DAG(
-        'n_kcgsa_ship-classification_dag',
+        'n_kcgsa_ship_shippred_dag',
         default_args={
             'owner': 'airflow',
             'start_date': datetime(2024, 12, 29),
         },
-        schedule_interval=timedelta(minutes=3)
-        #schedule_interval='@hourly',  # 매 시 실행
+        #schedule_interval=timedelta(minutes=3)
+        schedule_interval='@hourly',  # 매 시 실행
         catchup=False  # 과거 실행을 건너뛸지 여부 (False로 하면 과거 실행 건너뜀)
 ) as dag:
 
-    s05_task = PythonOperator(
-        task_id='s05',
-        python_callable=s05_api_call
+    s07_task = PythonOperator(
+        task_id='s07',
+        python_callable=s07_api_call
     )
 
     # api_task >> next_task
-    s05_task
+    s07_task
