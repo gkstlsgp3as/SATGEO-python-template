d2192da2a08aec1601ff755af55df844570aea6a
gkstlsgp3as <58411517+gkstlsgp3as@users.noreply.github.com>
2024-12-31 19:52:53 +0900

    Update kcgsa_dag_w_img.py

--- app/dags/kcgsa_dag_w_img.py
+++ app/dags/kcgsa_dag_w_img.py
@@ -6,12 +6,11 @@ from datetime import datetime
 
 host_path = 'http://ship-service.ship.svc.cluster.local:80/'
 
-# DCMS에서 영상 id가 kwargs를 통해 입력된다고 가정하고 작성하였습니다. 피드백 부탁드립니다. 
 def w04_async_api_call(**kwargs):
     ti = kwargs['ti']
     api_path = '/api/risk-mappings/w04'
     api_params = {
-        'satellite_sar_image_id': ti.satellite_sar_image_id  
+        'satellite_sar_image_id': 123456789
     }
 
     requests.get(host_path + api_path, params=api_params)
@@ -21,7 +20,7 @@ def w05_async_api_call(**kwargs):
     ti = kwargs['ti']
     api_path = '/api/risk-mappings/w05'
     api_params = {
-        'satellite_sar_image_id': ti.satellite_sar_image_id  
+        'satellite_sar_image_id': 123456789  
     }
 
     requests.get(host_path + api_path, params=api_params)
